c6fd151d62edc0fa857e28d583a6d824701de95d
RERE CHINTYA <rerechintya@student.telkomuniversity.ac.id>
2022-04-20 21:56:22 +0700

    mengubah comment
    mengubah comment menjadi code program

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -1,23 +1,24 @@
 package lib;
 
 public class TaxFunction {
-	private final int pajak = 5 / 100;
-	private final int single = 54000000;
-	private final int married = 4500000;
-	private final int kids = 3;
-	private final int marriedkids = kids * 4500000;
+	private static final int pajak = 5 / 100;
+	private static final int single = 54000000;
+	private static final int married = 4500000;
+	private static final int kids = 3;
+	private static final int bulan = 12;
+	private static final int marriedkids = kids * 4500000;
 
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible,
 			boolean isMarried, int numberOfChildren) {
 
 		int tax = 0;
 
-		if (numberOfMonthWorking > 12) {
+		if (numberOfMonthWorking > bulan) {
 			System.err.println("More than 12 month working per year");
 		}
 
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
+		if (numberOfChildren > kids) {
+			numberOfChildren = kids;
 		}
 
 		if (isMarried) {
